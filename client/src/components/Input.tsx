2112cf19b0b374f74feffdb539b27caf15ee380a
Thabish <kadertabish2@gmail.com>
2023-01-25 16:19:00 +0400

    custom-inputs
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,18 @@
+import React, {
+	DetailedHTMLProps,
+	InputHTMLAttributes,
+	forwardRef,
+} from "react";
+
+export const Input = forwardRef<
+	HTMLInputElement,
+	DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+>(({ className, ...rest }, ref) => {
+	return (
+		<input
+			className={`py-1 px-2 border border-gray-400 focus:border-blue-500 outline-none rounded w-full ${className}`}
+			{...rest}
+			ref={ref}
+		/>
+	);
+});
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Input } from "../components/Input";
 
 const SignUp = () => {
+	const nameRef = useRef<HTMLInputElement>(null);
 	return (
 		<div>
 			<h1 className="text-2xl font-bold text-center">Signup</h1>
 			<div className="grid grid-cols-[auto,1fr] gap-y-2 gap-x-3">
 				<label htmlFor="">Username</label>
-				<input type="text" />
+				<Input ref={nameRef} />
+
 				<label htmlFor="">Name</label>
-				<input type="text" />
+				<Input type="text" />
 				<label htmlFor="">Image Url</label>
-				<input type="text" />
+				<Input type="text" />
 			</div>
 		</div>
 	);
