755bf09a99585e9f74dab2280a4bcf948285d41f
Thabish <kadertabish2@gmail.com>
2023-01-25 16:25:18 +0400

    sign-up-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React, { DetailedHTMLProps, forwardRef } from "react";
+
+export const Button = forwardRef<
+	HTMLButtonElement,
+	DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	>
+>(({ className, children, ...rest }) => {
+	return (
+		<button
+			className={`border-2 border-gray-900 bg-blue-600 rounded p-2 w-full text-white hover:bg-blue-500 focus:bg-blue-400 transition-colors  disabled:bg-gray-500 ${className}`}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+});
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,21 +1,26 @@
 import React, { useRef } from "react";
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 const SignUp = () => {
+	const usernameRef = useRef<HTMLInputElement>(null);
 	const nameRef = useRef<HTMLInputElement>(null);
+	const imageUrlRef = useRef<HTMLInputElement>(null);
 	return (
-		<div>
-			<h1 className="text-2xl font-bold text-center">Signup</h1>
-			<div className="grid grid-cols-[auto,1fr] gap-y-2 gap-x-3">
-				<label htmlFor="">Username</label>
-				<Input ref={nameRef} />
-
-				<label htmlFor="">Name</label>
-				<Input type="text" />
-				<label htmlFor="">Image Url</label>
-				<Input type="text" />
-			</div>
-		</div>
+		<>
+			<h1 className="text-3xl font-bold mb-8 text-center">Sign Up</h1>
+			<form className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end">
+				<label htmlFor="userName">Username</label>
+				<Input id="name" required ref={usernameRef} />
+				<label htmlFor="name">Name</label>
+				<Input id="name" type="url" required ref={nameRef} />
+				<label htmlFor="imageUrl">Image Url</label>
+				<Input id="imageUrl" type="url" required ref={imageUrlRef} />
+				<Button type="submit" className="col-span-full">
+					Sign up
+				</Button>
+			</form>
+		</>
 	);
 };
 
