4ba6c41b97d1ea339778695fc2b5db112595f921
Thabish <kadertabish2@gmail.com>
2023-01-27 14:36:38 +0400

    routes
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,22 +3,34 @@ import AuthLayout from "./pages/layout/AuthLayout";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import { AuthProvider } from "./context/AuthContext";
+import { RootLayout } from "./pages/layout/RootLayout";
+import Home from "./pages/Home";
 
 export const router = createBrowserRouter([
 	{
 		element: <ContextWrapper />,
 		children: [
 			{
+				path: "/",
+				element: <RootLayout />,
+				children: [
+					{ index: true, element: <Home /> },
+					{
+						path: "channel",
+						children: [
+							{
+								path: "new",
+								element: <h1>New channel</h1>,
+							},
+						],
+					},
+				],
+			},
+			{
 				element: <AuthLayout />,
 				children: [
-					{
-						path: "login",
-						element: <Login />,
-					},
-					{
-						path: "signup",
-						element: <SignUp />,
-					},
+					{ path: "login", element: <Login /> },
+					{ path: "signup", element: <SignUp /> },
 				],
 			},
 		],
