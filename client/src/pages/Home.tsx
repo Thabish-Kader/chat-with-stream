566fc3d102be9b3c2acab76c374d6e4a30bc3120
Thabish <kadertabish2@gmail.com>
2023-01-27 14:38:02 +0400

    hone
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -41,14 +41,41 @@ function Channels({ loadedChannels }: ChannelListMessengerProps) {
 	const { setActiveChannel, channel: activeChannel } = useChatContext();
 
 	return (
-		<div className="w-60 flex flex-col gap-4 ,-3 h-full">
+		<div className="w-60 flex flex-col gap-4 m-3 h-full">
 			<Button onClick={() => navigate("/channel/new")}>
-				New Coversation
+				New Conversation
 			</Button>
 			<hr className="border-gray-500" />
 			{loadedChannels != null && loadedChannels.length > 0
-				? "test"
-				: "No conversation"}
+				? loadedChannels.map((channel) => {
+						const isActive = channel === activeChannel;
+						const extraClasses = isActive
+							? "bg-blue-500 text-white"
+							: "hover:bg-blue-100 bg-gray-100";
+						return (
+							<button
+								onClick={() => setActiveChannel(channel)}
+								disabled={isActive}
+								className={`p-4 rounded-lg flex gap-3 items-center ${extraClasses}`}
+								key={channel.id}
+							>
+								{channel.data?.image && (
+									<img
+										src={channel.data.image}
+										className="w-10 h-10 rounded-full object-center object-cover"
+									/>
+								)}
+								<div className="text-ellipsis overflow-hidden whitespace-nowrap">
+									{channel.data?.name || channel.id}
+								</div>
+							</button>
+						);
+				  })
+				: "No Conversations"}
+			<hr className="border-gray-500 mt-auto" />
+			{/* <Button onClick={() => logout.mutate()} disabled={logout.isLoading}>
+				Logout
+			</Button> */}
 		</div>
 	);
 }
