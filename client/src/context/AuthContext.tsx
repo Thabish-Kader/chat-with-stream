bc10aa230c721a71586d37833dc245a8bd76c31b
Thabish <kadertabish2@gmail.com>
2023-01-27 10:16:22 +0400

    chat-connection-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,12 +1,14 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useMutation, UseMutationResult } from "@tanstack/react-query";
 import axios, { AxiosResponse } from "axios";
 import { useNavigate } from "react-router-dom";
+import { StreamChat } from "stream-chat";
 
 type AuthContext = {
 	signup: UseMutationResult<AxiosResponse, unknown, User>;
 	login: UseMutationResult<{ token: string; user: User }, unknown, string>;
 	user?: User;
+	streamChat?: StreamChat;
 };
 const Context = createContext<AuthContext | null>(null);
 type User = {
@@ -20,6 +22,8 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 	const [user, setUser] = useState<User>();
+	const [token, setToken] = useState("");
+	const [streamChat, setStreamChat] = useState<StreamChat>();
 	const navigate = useNavigate();
 	const signup = useMutation({
 		mutationFn: (user: User) => {
@@ -41,9 +45,33 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 			);
 			return res.data as { token: string; user: User };
 		},
+		onSuccess(data, variables, context) {
+			setUser(data.user);
+			setToken(data.token);
+		},
 	});
+	useEffect(() => {
+		if (token == null || user == null) return;
+		const chat = new StreamChat(import.meta.env.VITE_STREAM_API_KEY);
+
+		if (chat.tokenManager.token === token && chat.userID === user.id)
+			return;
+
+		let isInterrupted = false;
+		const connectPromise = chat.connectUser(user, token).then(() => {
+			if (isInterrupted) return;
+			setStreamChat(chat);
+		});
+		return () => {
+			isInterrupted = true;
+			setStreamChat(undefined);
+			connectPromise.then(() => {
+				chat.disconnectUser();
+			});
+		};
+	}, [token, user]);
 	return (
-		<Context.Provider value={{ signup, login, user }}>
+		<Context.Provider value={{ signup, login, user, streamChat }}>
 			{children}
 		</Context.Provider>
 	);
